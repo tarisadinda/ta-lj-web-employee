30711a3ca11f677b523069c2000fdb5a296567e6
tarisadinda20 <tarisadinda20@gmail.com>
2023-07-01 15:50:46 +0700

    add application page view

---
+++ src/pages/my-application.js
@@ -0,0 +1,29 @@
+import LayoutMain from "@/components/layouts/main"
+import styles from '@/styles/pages/MyApplication.module.scss'
+import ApplicationCard from "@/components/my-application/application-card"
+import SideMenu from "@/components/profile/side-menu"
+
+export default function MyApplication() {
+    return(<>
+        <h3 className="mb-4"><b>Daftar Lamaran Saya</b></h3>
+        <div className={styles.colGroup}>
+            <SideMenu />
+            <div className={styles.cardList}>
+                <ApplicationCard />
+                <ApplicationCard />
+                <ApplicationCard />
+                <ApplicationCard />
+                <ApplicationCard />
+                <ApplicationCard />
+            </div>
+        </div>
+    </>)
+}
+
+MyApplication.getLayout = function getLayout(page) {
+    return (
+        <LayoutMain>
+            {page}
+        </LayoutMain>
+    )
+}
