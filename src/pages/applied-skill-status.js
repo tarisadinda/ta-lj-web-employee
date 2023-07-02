bfadc07c392b371d4c0e360495b246df0024e1fd
tarisadinda20 <tarisadinda20@gmail.com>
2023-07-02 15:49:11 +0700

    add new page

---
+++ src/pages/applied-skill-status.js
@@ -0,0 +1,28 @@
+import SidemenuLayout from "@/components/layouts/side-menu-layout";
+import ApplyStatusCard from "@/components/my-skills/apply-status-card";
+import Link from "next/link";
+
+export default function AppliedSkillStatus() {
+    return(<>
+        <h3 className="mb-4"><b>Status Pengajuan Keahlian</b></h3>
+        <div style={{ display: 'grid', rowGap: '15px' }}>
+            <Link href='/apply-skill-detail'>
+                <ApplyStatusCard />
+            </Link>
+            <Link href='/apply-skill-detail'>
+                <ApplyStatusCard />
+            </Link>
+            <Link href='/apply-skill-detail'>
+                <ApplyStatusCard />
+            </Link>
+        </div>
+    </>)
+}
+
+AppliedSkillStatus.getLayout = function getLayout(page) {
+    return (
+        <SidemenuLayout>
+            {page}
+        </SidemenuLayout>
+    )
+}
